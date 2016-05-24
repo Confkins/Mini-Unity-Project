816f03274b6d5560780825ef837ad7a849266677
Oisin Redmond <Oisin Redmond>
2016-05-24 23:49:59 +0100

    He can move!

--- Conoisin Unity Project/Assets/Player_Controller_1.cs
+++ Conoisin Unity Project/Assets/Player_Controller_1.cs
@@ -4,35 +4,54 @@ using System.Collections;
 public class Player_Controller_1 : MonoBehaviour {
 
 	public bool jump = false;
-	public float speed = 5;
-	public float jumpForce = 1000f;
+	public float jump_force = 1000f;
+	public float max_speed = 5f;
+	public float move_force = 365f;
 
-	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update ()
+	{
+		Jump_Detect ();
 	}
 
 	void FixedUpdate ()
 	{
-		Controller ();
-		if (jump) {
-			GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
+		Movement ();
+		Jump ();	
+	}
 
-
-
-
+	void Jump_Detect()
+	{
+		if (Input.GetButtonDown ("Jump") == true) 
+		{
+			jump = true;
 		}
 	}
-	void Controller () {
-		if (Input.GetButtonDown("Jump") == true) {
-			jump = true;
-		    	
+
+	void Jump()
+	{
+		if (jump)
+		{
+			GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0f, jump_force));
+			jump = false;
+		}
 	}
-}
+
+	void Movement()
+	{
+		float h = Input.GetAxis ("Horizontal");
+		if (h * GetComponent<Rigidbody2D> ().velocity.x < max_speed) {
+			GetComponent<Rigidbody2D> ().AddForce (Vector2.right * h * move_force); 
+		}
+		if (Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) > max_speed) {
+			GetComponent<Rigidbody2D> ().velocity = 
+			new Vector2 (Mathf.Sign (GetComponent<Rigidbody2D> ().velocity.x)
+			* max_speed, GetComponent<Rigidbody2D> ().velocity.y);
+		}
+	}
 }
 
