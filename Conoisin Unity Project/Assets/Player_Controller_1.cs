2dcc05949a93b6262e66e3cd9a95be505be32468
Oisin Redmond <Oisin Redmond>
2016-05-24 22:22:54 +0100

    Jumping

---
+++ Conoisin Unity Project/Assets/Player_Controller_1.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Controller_1 : MonoBehaviour {
+
+	public bool jump = false;
+	public float speed = 5;
+	public float jumpForce = 1000f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate ()
+	{
+		Controller ();
+		if (jump) {
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
+
+
+
+
+		}
+	}
+	void Controller () {
+		if (Input.GetButtonDown("Jump") == true) {
+			jump = true;
+		    	
+	}
+}
+}
+
